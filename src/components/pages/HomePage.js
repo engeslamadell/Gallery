e6d4d96845739938fc5686a19f25b41e3dbae079
Eslam Adel <eslamadel72@outlook.com>
2020-02-22 22:20:37 +0200

    adding mock data to db.json and getting some image from an api and simulate the process of fetching the data using axios and useEffect with no redux yet

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import Card from '../ui/Card';
@@ -9,10 +9,7 @@ import Button from '../ui/Button';
 import Heading from '../ui/Heading';
 import ServicesCard from '../ui/ServicesCard';
 import Paragraph from '../ui/Paragraph';
-
-import one from '../../assets/1.jpg';
-import two from '../../assets/2.jpg';
-import three from '../../assets/3.jpg';
+import axios from '../../api/getData';
 
 import icon1 from '../../assets/icons/icon1.png';
 import icon2 from '../../assets/icons/icon2.png';
@@ -47,100 +44,94 @@ const SideSectionContainer = styled.div`
   margin-right: 2%;
 `;
 
-const HomePage = () => (
-  <div>
-    <ContentContainer>
-      <SideSectionContainer>
-        <Button isNotLast isActive={false} buttonText="Clear" />
-        <Button isActive buttonText="Apply" />
-        <TextInput />
-      </SideSectionContainer>
-      <CardContainer>
-        <Card>
-          <Image imageSrc={one} isFullWidth imageHeight="100%"/>
-          <CardTitle>Wedding details</CardTitle>
-        </Card>
-        <Card>
-          <Image imageSrc={two} isFullWidth imageHeight="100%"/>
-          <CardTitle>Gold/marble wedding cake</CardTitle>
-        </Card>
-        <Card>
-          <Image imageSrc={three} isFullWidth imageHeight="100%"/>
-          <CardTitle>Gold frame Invitation set</CardTitle>
-        </Card>
+const HomePage = () => {
+  const [data, setData] = useState([]);
 
-        <Card>
-          <Image imageSrc={one} isFullWidth imageHeight="100%"/>
-          <CardTitle>Wedding details</CardTitle>
-        </Card>
-        <Card>
-          <Image imageSrc={two} isFullWidth imageHeight="100%"/>
-          <CardTitle>Gold/marble wedding cake</CardTitle>
-        </Card>
-        <Card>
-          <Image imageSrc={three} isFullWidth imageHeight="100%"/>
-          <CardTitle>Gold frame Invitation set</CardTitle>
-        </Card>
-      </CardContainer>
-    </ContentContainer>
-    <Heading isCenter isMainHeading>Wedding Planning Is a peice of cake with weds360. We know what matters most and you can count on us every step of your way.</Heading>
-    <AboutWeds>
-      <ServicesCard>
-        <ServicesCardImage>
-            <Image imageSrc={icon1} />
-        </ServicesCardImage>
-        <div>
-            <Heading fontSize="true">WEDDING WEBSITE</Heading>
-            <Paragraph>Do you have a figure in mind? Place it in the budgeter and we will provide you with all the choices and possibilities you can think of.</Paragraph>
-        </div>
-      </ServicesCard>
-      <ServicesCard>
-        <ServicesCardImage>
-            <Image imageSrc={icon2} />
-        </ServicesCardImage>
-        <div>
-            <Heading fontSize="true">BUDGETER</Heading>
-            <Paragraph>Set your tasks, create your priority list and never forget any wedding detail.</Paragraph>
-        </div>
-      </ServicesCard>
-      <ServicesCard>
-        <ServicesCardImage>
-            <Image imageSrc={icon3} />
-        </ServicesCardImage>
-        <div>
-            <Heading fontSize="true">CHECK LIST</Heading>
-            <Paragraph>Who is coming? Who is not? Who replied with a "Maybe"? Manage all of this while customizing your invitations. All in one place and right at your fingertips.</Paragraph>
-        </div>
-      </ServicesCard>
-      <ServicesCard>
-        <ServicesCardImage>
-            <Image imageSrc={icon4} />
-        </ServicesCardImage>
-        <div>
-            <Heading fontSize="true">GUEST LIST</Heading>
-            <Paragraph>Meet your service providers, view their profiles and make all the perfect choice for your wedding night.</Paragraph>
-        </div>
-      </ServicesCard>
-      <ServicesCard>
-        <ServicesCardImage>
-            <Image imageSrc={icon5} />
-        </ServicesCardImage>
-        <div>
-            <Heading fontSize="true">VENDORS</Heading>
-            <Paragraph>Escape the awkward moments and receiving the same gift twice by listing everything you've thought of and every store you love. Let your guests choose what to get you for the wedding and say goodbye to unwanted presents.</Paragraph>
-        </div>
-      </ServicesCard>
-      <ServicesCard>
-        <ServicesCardImage>
-            <Image imageSrc={icon6} />
-        </ServicesCardImage>
-        <div>
-            <Heading fontSize="true">REGISTRY LIST</Heading>
-            <Paragraph>Where you will save all your wedding memories forever, where you will choose your own personalized template, manage every event detail and let your guests share their pictures, videos and moments with you.</Paragraph>
-        </div>
-      </ServicesCard>
-    </AboutWeds>
-  </div>
-);
+  useEffect(() => {
+    const getData = async () => {
+      const result = await axios.get('http://localhost:3001/data');
+      setData(result.data);
+    }
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <ContentContainer>
+        <SideSectionContainer>
+          <Button isNotLast isActive={false} buttonText="Clear" />
+          <Button isActive buttonText="Apply" />
+          <TextInput />
+        </SideSectionContainer>
+        <CardContainer>
+          { data.map(item => (
+            <Card key={item.id}>
+              <Image imageSrc={item.photoPath} isFullWidth imageHeight="100%"/>
+              <CardTitle>{item.title}</CardTitle>
+            </Card>
+          )) }
+        </CardContainer>
+      </ContentContainer>
+      <Heading isCenter isMainHeading>Wedding Planning Is a peice of cake with weds360. We know what matters most and you can count on us every step of your way.</Heading>
+      <AboutWeds>
+        <ServicesCard>
+          <ServicesCardImage>
+              <Image imageSrc={icon1} />
+          </ServicesCardImage>
+          <div>
+              <Heading fontSize="true">WEDDING WEBSITE</Heading>
+              <Paragraph>Do you have a figure in mind? Place it in the budgeter and we will provide you with all the choices and possibilities you can think of.</Paragraph>
+          </div>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesCardImage>
+              <Image imageSrc={icon2} />
+          </ServicesCardImage>
+          <div>
+              <Heading fontSize="true">BUDGETER</Heading>
+              <Paragraph>Set your tasks, create your priority list and never forget any wedding detail.</Paragraph>
+          </div>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesCardImage>
+              <Image imageSrc={icon3} />
+          </ServicesCardImage>
+          <div>
+              <Heading fontSize="true">CHECK LIST</Heading>
+              <Paragraph>Who is coming? Who is not? Who replied with a "Maybe"? Manage all of this while customizing your invitations. All in one place and right at your fingertips.</Paragraph>
+          </div>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesCardImage>
+              <Image imageSrc={icon4} />
+          </ServicesCardImage>
+          <div>
+              <Heading fontSize="true">GUEST LIST</Heading>
+              <Paragraph>Meet your service providers, view their profiles and make all the perfect choice for your wedding night.</Paragraph>
+          </div>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesCardImage>
+              <Image imageSrc={icon5} />
+          </ServicesCardImage>
+          <div>
+              <Heading fontSize="true">VENDORS</Heading>
+              <Paragraph>Escape the awkward moments and receiving the same gift twice by listing everything you've thought of and every store you love. Let your guests choose what to get you for the wedding and say goodbye to unwanted presents.</Paragraph>
+          </div>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesCardImage>
+              <Image imageSrc={icon6} />
+          </ServicesCardImage>
+          <div>
+              <Heading fontSize="true">REGISTRY LIST</Heading>
+              <Paragraph>Where you will save all your wedding memories forever, where you will choose your own personalized template, manage every event detail and let your guests share their pictures, videos and moments with you.</Paragraph>
+          </div>
+        </ServicesCard>
+      </AboutWeds>
+    </div>
+  )
+}
 
 export default HomePage;
